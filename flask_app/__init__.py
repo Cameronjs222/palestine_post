50d063e0f5806784abbb666c0aa42f16e718cd84
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-11-07 15:26:58 -0800

    Generated file structure and hello world

---
+++ flask_app/__init__.py
@@ -0,0 +1,3 @@
+from flask import Flask
+app = Flask(__name__)
+app.secret_key = "I'm going to be king of the pirates"
---
+++ server.py
@@ -0,0 +1,6 @@
+from flask_app import app
+# from flask_app.Controllers import User_routes
+from flask_app.Controllers import Post_routes
+
+if __name__=="__main__":
+    app.run(debug=True, port=5000)
