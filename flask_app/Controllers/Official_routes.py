6f15ec010c14890710cc28df6d551c647ac424c8
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-11-13 14:14:54 -0800

    Added sorting for states

--- flask_app/Controllers/Official_routes.py
+++ flask_app/Controllers/Official_routes.py
@@ -4,5 +4,19 @@ from flask_app.Models.Official_models import Official
 
 @app.route('/')
 def index():
-    # officials = Official.find_all_officials()
-    return render_template("search.html")
+    officials = Official.find_all_officials()
+    officials = Official.find_all_officials()
+    all_states_set = set()
+
+    for official in officials:
+        official_state = official.state[:2]  # Using slicing for the first two characters
+        if (official_state == 'US'):
+            continue
+        all_states_set.add(official_state)
+
+        all_states_list = sorted(list(all_states_set))
+        count = len(all_states_list)
+
+    print(all_states_list)
+    print(count)
+    return render_template("search.html" , officials = officials, all_states = all_states_list, count = count)
