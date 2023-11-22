4b80f83f3e441eab707771d49e459fa1865037c4
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-11-21 19:14:41 -0800

    alert div create and functioning

--- flask_app/Static/script.js
+++ flask_app/Static/script.js
@@ -13,3 +13,32 @@ function filterFunction(list_container, input_label) {
       }
     }
   }
+
+  function alertFunction(btnId, btnTitle, official, alertDiv) {
+    const appendAlert = (message, type) => {
+        console.log("creating div");
+        const wrapper = document.getElementById(alertDiv);
+        wrapper.innerHTML = ''
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" id="${btnId}_alert" role="alert">`,
+            `   <div>${message}</div>`,
+            `   <button type="button" class="btn-close" aria-label="Close" onclick="closeAlert('${btnId}_alert')"></button>`,
+            '</div>'
+        ].join('');
+    };
+
+    const alertTrigger = document.getElementById(btnId);
+    if (alertTrigger) {
+        alertTrigger.addEventListener('click', () => {
+            appendAlert(`${official}'s ${btnTitle} account is not linked. Please check back later`, 'success');
+        });
+    }
+}
+
+function closeAlert(id) {
+    const alert = document.getElementById(id);
+    if (alert) {
+        alert.remove();
+        console.log('done');
+    }
+}
