5da15be3f55f89acb562290177ad0f490bdccb15
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-11-29 18:21:07 -0800

    added test controller to server

--- server.py
+++ server.py
@@ -2,6 +2,6 @@ from flask_app import app
 # from flask_app.Controllers import User_routes
 from flask_app.Controllers import Post_routes
 from flask_app.Controllers import Official_routes
-
+from flask_app.Test_Controllers import scrapper_test
 if __name__=="__main__":
     app.run(debug=True, port=5000)
