b315206bc8d03a3745f0c3391bd689f66f126d3c
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-12-04 14:15:29 -0800

    twitter scrape working, but not updating

--- flask_app/Controllers/Official_routes.py
+++ flask_app/Controllers/Official_routes.py
@@ -1,8 +1,11 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.Models.Official_models import Official
+from flask_app.Models.Post_models import Post
 from flask_app.Models.api_models import twitter_scrape, get_congress_members, create_official_and_posts
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import concurrent.futures
 
 @app.route('/admin')
@@ -33,7 +36,7 @@ def show_state(state):
     return render_template("states.html", officials = officials)
 
 @app.route('/official/congress/get', methods=['POST', 'GET'])
-def create_post():
+def get_congress():
     print(request.form)
     try:
         congress = int(request.form['select_term'])
@@ -46,40 +49,78 @@ def create_post():
                 official_id = Official.find_officials_by_name(member['first_name'], member['last_name'])
                 if official_id:
                     print(official_id)
-                    Official.update_official(member, official_id)
+                    for member_id in official_id:
+                        Official.update_official(member, member_id.id)
                 else:
                     print(member)
                     Official.create_official(member)
         return redirect(f'/admin?congress={congress}&chamber={chamber}&full_congress=True')
     except KeyError as e:
-        print("KeyError" + str(e))
+        print("KeyError " + str(e))
         chamber = request.form['select_chamber']
         return redirect(f'/admin?chamber={chamber}&full_congress=False')
     except ValueError as e:
         print("ValueError" + str(e))
         # Handle the case where 'select_term' cannot be converted to an integer
         return redirect('/admin?full_congress=False')
-    
+
 @app.route('/official/twitter/update', methods=['POST', 'GET'])
 def update_twitter():
     print(request.form)
     date = request.form['date']
-    
+    limit = int(request.form['limit'])
+    congress_limit = int(request.form['congress_limit'])
+    full_congress = Official.find_all_officials()[:congress_limit]
+
+    if full_congress:
+        # Use ThreadPoolExecutor to parallelize API calls
+        with ThreadPoolExecutor() as executor:
+            # Use list comprehension to submit tasks
+            futures = [
+                executor.submit(process_member, member, date, limit) for member in full_congress
+            ]
+
+    return "done"
+
+
+
+def process_member(member, date, limit):
     try:
-        full_congress = Official.find_all_officials()
-        if full_congress:
-            # Use ThreadPoolExecutor to parallelize API calls
-            with ThreadPoolExecutor() as executor:
-                # Use list comprehension to submit tasks
-                futures = [executor.submit(twitter_scrape, member.twitter_account, date) for member in full_congress]
+        # Call twitter_scrape and capture the result
+        tweets = twitter_scrape(member.twitter_account, date, limit)
+        
+        for tweet in tweets:
+            print(f"Processing tweet: {tweet['post_id']}, user: {tweet['username']}")
+            post_data = {
+                'post_avatar': tweet['post_avatar'],
+                'url': tweet['url'],
+                'query': tweet['query'],
+                'post_id': tweet['post_id'],
+                'text': tweet['text'],
+                'username': tweet['username'],
+                'fullname': tweet['fullname'],
+                'timestamp': tweet['timestamp'],
+                'replies': tweet['replies'],
+                'reposts': tweet['reposts'],
+                'likes': tweet['likes'],
+                'quotes': tweet['quotes'],
+                'official_id': member.id,
+            }
+
+            try:
+                print(f"Creating post for {member.first_name} {member.last_name}")
+                post_id = Post.create_post(post_data, member.id)
+                print(f"post created with ID: {post_id}")
                 
-                # Wait for all tasks to complete
-                concurrent.futures.wait(futures)
-            
-        return redirect(f'/admin?full_congress=True')
-    except KeyError:
-        chamber = request.form['select_chamber']
-        return redirect(f'/admin?chamber={chamber}&full_congress=False')
-    except ValueError:
-        # Handle the case where 'select_term' cannot be converted to an integer
-        return redirect('/admin?full_congress=False')
+                if len(tweet['images']) > 0:
+                    print(f"Adding images to post for {member.first_name} {member.last_name}")
+                    for image in tweet['images']:
+                        image_data = {'image_url': image}
+                        Post.add_post_images(image_data, post_id)
+                        print(f"Image added to post")
+            except Exception as e:
+                print(f"Error creating post: {str(e)}")
+        
+        return {"success": True, "member_id": member.id}
+    except Exception as e:
+        return {"success": False, "member_id": member.id, "error": str(e)}
