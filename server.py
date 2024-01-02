be73cfe09746c079d7c4963e24a553a29988652e
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2024-01-01 23:45:35 -0800

    added user routes to server.py

--- server.py
+++ server.py
@@ -3,5 +3,6 @@ from flask_app import app
 from flask_app.controllers import Post_routes
 from flask_app.controllers import Official_routes
 from flask_app.test_controllers import scrapper_test
+from flask_app.controllers import User_routes
 if __name__=="__main__":
     app.run(debug=True, port=5000)
