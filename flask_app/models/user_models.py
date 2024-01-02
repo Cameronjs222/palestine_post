2543cefaa3af4eca5d4f0bda337728c8bcd3bef6
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2024-01-01 23:48:24 -0800

    user models

--- flask_app/models/user_models.py
+++ flask_app/models/user_models.py
@@ -4,7 +4,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from bcrypt import checkpw, gensalt, hashpw
 
 class User():
-    my_db = 'palistine_posts'
+    my_db = 'palestine_tweets'
     def __init__(self, data):
         self.id = data['id']
         self.username = data['username']
@@ -13,18 +13,30 @@ class User():
         self.updated_at = data['updated_at']
 
     @classmethod
-    def create_user(cls, data):
+    def create_user(cls, form_data):
         query = """
-        INSERT INTO user (username, password)
+        INSERT INTO users (username, password)
         VALUES (%(username)s, %(password)s)
         """
+
+        if form_data['password'] != form_data['confirm_password']:
+            return False
+        
+        data = {
+            'username': form_data['username'],
+            'password': hashpw(form_data['password'].encode(), gensalt()).decode()
+        }
         return connectToMySQL(cls.my_db).query_db(query, data)
     
     @classmethod
-    def get_user_by_username(cls, data):
-        query = "SELECT * FROM user WHERE username = %(username)s;"
+    def get_user_by_username(cls, form_data):
+        query = "SELECT * FROM users WHERE username = %(username)s;"
+
+        data = {
+            'username': form_data['username']
+        }
         result = connectToMySQL(cls.my_db).query_db(query, data)
-        if len(result) < 1:
+        if result == False:
             return False
         return cls(result[0])
     
@@ -32,5 +44,7 @@ class User():
     def validate_login(data):
         user = User.get_user_by_username(data)
         if not user:
+            return False
+        if not checkpw(data['password'].encode(), user.password.encode()):
             return False
         return True
